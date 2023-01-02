bbbd9b62599a4ada547b2bb4c39e3ae5ddf06f93
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-01-02 09:58:18 +0100

    fixed

--- components/header.js
+++ components/header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { Badge } from "primereact/badge";
 
 export default function header() {
   return (
@@ -8,6 +9,9 @@ export default function header() {
         <div className="header__logo">Cv Generator</div>
         <div className="header__links">
           <Link className="btn__secondary" href="/cover-letter">
+            <div className="coming__soon">
+              <Badge value="coming soon" severity="danger"></Badge>
+            </div>
             Cover Letter
           </Link>
           <Link className="btn__primary" href="/generator">
