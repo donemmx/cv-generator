4dd09edb67ff23892ccbad557a05ba65306ba254
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-01-02 10:14:23 +0100

    fixed

--- pages/generator/index.js
+++ pages/generator/index.js
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
-import PersonalDetails from '../../components/forms/personalDetail'
-import ProfessionalSummary from '../../components/forms/professioanalSumary'
-import Education from '../../components/forms/education'
-import EmploymentHistory from '../../components/forms/employmentHistory'
-import Website from '../../components/forms/website'
-import Skills from '../../components/forms/skillComponent'
-import Reference from '../../components/forms/reference'
-import Hobbies from '../../components/forms/hobbies'
-import Languages from '../../components/forms/languages'
-
-
+import PersonalDetails from "../../components/forms/personalDetail";
+import ProfessionalSummary from "../../components/forms/professioanalSumary";
+import Education from "../../components/forms/education";
+import EmploymentHistory from "../../components/forms/employmentHistory";
+import Website from "../../components/forms/website";
+import Skills from "../../components/forms/skillComponent";
+import Reference from "../../components/forms/reference";
+import Hobbies from "../../components/forms/hobbies";
+import Languages from "../../components/forms/languages";
 
 export default function index() {
   const [title, setTitle] = useState("Untitled");
@@ -55,7 +53,21 @@ export default function index() {
           </div>
         </div>
         <div className="generator__right">
-          <div className="preview"></div>
+          <div className="preview">
+            <div className="preview__body"></div>
+          </div>
+          <div className="preview__panel">
+            <div className="preview_buttons">
+              <button className="btn__secondary">
+                <i className="pi pi-th-large"></i>
+                Select Template
+              </button>
+              <button className="btn__secondary">
+                <i className="pi pi-download"></i>
+                Download
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
